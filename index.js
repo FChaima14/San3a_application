754de17d0b38f709273d0343ce0d0055088b464d
FChaima14 <107279266+FChaima14@users.noreply.github.com>
2022-07-06 22:01:36 +0100

    configuration

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ app.get('/', (req, res)=>{
      res.send('hello to San3a api')
 })
 //databse connection
+CONECTION_URL='mongodb+srv://chaima:chaima104@cluster0.rvmoq.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT|| 5000;
-app.listen(PORT, ()=>console.log(`Server Running on port :${PORT} `))
-//mongoose.connect(process.env.CONECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-     //.then(()=> app.listen(PORT, ()=>console.log(`Server Running on port :${PORT} `)))
-    // .catch((error)=> console.log(error.message));
+//app.listen(PORT, ()=>console.log(`Server Running on port :${PORT} `))
+mongoose.connect(CONECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+     .then(()=> app.listen(PORT, ()=>console.log(`Server Running on port :${PORT} `)))
+     .catch((error)=> console.log(error.message));
 
 
